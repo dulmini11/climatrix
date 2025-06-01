2f94c72b1a9cabaee6630e89cbe25c46e7e7f37d
dulmini11 <dulminihw2@gmail.com>
2025-06-01 20:17:25 +0530

    Add Temperature section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Navbar from "./Components/Navbar";
 import { format, parseISO } from 'date-fns';
 
 import axios from "axios";
+import Container from "./Components/Container";
+import { convertKelvinTocelsius } from "./utils/convertKelvinTocelsius";
 
 //... other type declarations
 
@@ -34,12 +36,36 @@ export default function Home() {
       <Navbar />
       <main className="px-3 max-w-7xl mx-auto flex-col gap-9 w-full pd-10 pt-4">
         {/*today*/}
-        <section>
-          <div>
+        <section className="space-y-4">
+          <div className="space-y-2">
             <h2 className="flex gap-1 text-2xl items-end">
               <p>{format(parseISO(firstData?.dt_txt ??''),'EEEE')}</p>
               <p className="text-lg">({format(parseISO(firstData?.dt_txt ??''),'dd.MM.yyyy')})</p>
             </h2>
+            <Container className="gap-10 px-6 items-center">
+              <div className="flex flex-col px-4">
+                <span className="text-5xl">
+                  {convertKelvinTocelsius(firstData?.main.temp ?? 296.37)}°
+                </span>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  <span>Feels like</span>
+                  <span>{convertKelvinTocelsius(firstData?.main.feels_like ?? 0)}°
+                  </span> 
+                  <p className="text-xs space-x-2">
+                    <span>
+                  {convertKelvinTocelsius(firstData?.main.temp_min ?? 0)}°↓{""}
+                </span>
+                <span>
+                  {""}
+                  {convertKelvinTocelsius(firstData?.main.temp_max ?? 0)}°↑
+                </span>
+                  </p>
+                  {/* <span>Feels like</span>
+                  <span>{convertKelvinTocelsius(firstData?.main.feels_like ?? 0)}°
+                  </span> */}
+                </p>
+              </div>
+            </Container>
           </div>
         </section>
         {/*7 day forcast data*/}
