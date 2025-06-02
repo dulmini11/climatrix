58ce02925b194278dcceab2fc0e046a81fbab578
dulmini11 <dulminihw2@gmail.com>
2025-06-02 23:47:39 +0530

    Create Forecast Weather Detail right side section

--- src/app/Components/ForecastWeatherDetail.tsx
+++ src/app/Components/ForecastWeatherDetail.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Container from './Container'
 import WeatherIcon from './WeatherIcon'
 import { WeatherDetailProps } from './WeatherDetails';
+import { convertKelvinTocelsius } from '../utils/convertKelvinTocelsius';
+import { MdDescription } from 'react-icons/md';
 
 export interface ForecastWeatherDetailProps extends WeatherDetailProps{
   visibility?: string;
@@ -30,6 +32,14 @@ export default function ForecastWeatherDetail( props : ForecastWeatherDetailProp
           <WeatherIcon iconName='={weatherIcon}'/>
           <p>{date}</p>
           <p className='text-sm'>{day}</p>
+        </div>
+
+        <div className='flex flex-col px-4'>
+          <span className='text-5xl'>{convertKelvinTocelsius(temp ?? 0)}°</span>
+          <p className='text-xs space-x-1 whitespace-nowrap'></p>
+          <span>Feels like</span>
+          <span>{convertKelvinTocelsius(feels_like ?? 0)}°</span>
+          <p className='capitalixe'>{description}</p>
         </div>
       </section>
     </Container>
