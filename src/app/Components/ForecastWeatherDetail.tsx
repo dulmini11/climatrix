7d39c08a2305160f2e1485db5be8806aec1a15c7
dulmini11 <dulminihw2@gmail.com>
2025-06-02 23:31:34 +0530

    Update Forecast Weather Detail file

--- src/app/Components/ForecastWeatherDetail.tsx
+++ src/app/Components/ForecastWeatherDetail.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import Container from './Container'
+import WeatherIcon from './WeatherIcon'
+import { WeatherDetailProps } from './WeatherDetails';
 
-type Props = {}
+export interface ForecastWeatherDetailProps extends WeatherDetailProps{
+  visibility?: string;
+  humidity?: string;
+  windSpeed?: string;
+  airPressure?: string;
+  sunrise?: string;
+  sunset?: string;
+}
 
-export default function ForecastWeatherDetail({}: Props) {
+
+export default function ForecastWeatherDetail( props : ForecastWeatherDetailProps) {
+  const{  
+  visibility = "25 km",
+  humidity = "61%",
+  windSpeed = "7 km/h",
+  airPressure = "1012 hPa",
+  sunrise = "6:20",
+  sunset = "18:48"
+} = props;
+
   return (
-    <Container>ForecastWeatherDetail</Container>
+    <Container className='gap-4'>
+      <section className='flex gap-4 items-center px-4'>
+        <div>
+          <WeatherIcon iconName='={weatherIcon}'/>
+          <p>{date}</p>
+          <p className='text-sm'>{day}</p>
+        </div>
+      </section>
+    </Container>
   )
 }
